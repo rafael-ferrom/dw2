d76c007c2d99d7aa3f9871286ca0d96b1ff69298
rafael-ferrom <118323003+rafael-ferrom@users.noreply.github.com>
2024-03-05 15:38:08 -0300

    Add files via upload

---
+++ rodape.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rodape',
+  templateUrl: './rodape.component.html',
+  styleUrl: './rodape.component.scss'
+})
+export class RodapeComponent {
+
+}
